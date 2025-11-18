2e2e2325caa7d0378fb933b7e18bc421226e26be
comma-elsewhere <studioclear2@gmail.com>
2025-11-18 14:55:40 -0700

    neighborhood done

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -7,12 +7,22 @@ extends Character
 signal entered_game(game_name:String)
 signal exited_game()
 
+var store_area: Area3D = null
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and store_area:
+		if store_area.has_method("disable_minute"):
+			store_area.disable_minute()
+			store_area = null
+
 
 func _on_detection_zone_area_entered(area: Area3D) -> void:
 	if area.is_in_group("Minigame"):
 		entered_game.emit(area.game_name)
+		store_area = area
 
 
 func _on_detection_zone_area_exited(area: Area3D) -> void:
 	if area.is_in_group("Minigame"):
 		exited_game.emit()
+		store_area = null
--- scripts/utility/game_zone.gd
+++ scripts/utility/game_zone.gd
@@ -1,3 +1,15 @@
 extends Area3D
 
+const DISABLED := 180.0
+
+@onready var collision: CollisionShape3D = $CollisionShape3D
+
 @export_enum("Workspace", "SellMerch", "Perform", "Busk", "Advertise", "DAW") var game_name: String
+
+func disable_minute() -> void:
+	if game_name == "Busk":
+		visible = false
+		collision.disabled = true
+		await get_tree().create_timer(DISABLED).timeout
+		visible = true
+		collision.disabled = false
